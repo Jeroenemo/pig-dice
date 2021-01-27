4d1e83f63ce6abae699fdddf4ef11fe9894595fb
Jeroen van Seeters <vanseetersjeroen@gmail.com>
2021-01-27 14:22:59 -0800

    add hold button and checkWinner
    Co-Authored-By: Mikey Kriegel <mikkrieg@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,99 @@
+//Business Logics
+
 function Dice(roll) {
   this.roll = roll
-}
-
-
+};
 Dice.prototype.rollDice = function() {
   return Math.floor(Math.random() * (Math.floor(6) - Math.ceil(1) + 1)) + 1;
+};
+
+function Player(name){
+  this.name = name;
+  this.turnScore = 0;
+  this.totalBank = 0;
+};
+
+Player.prototype.checkWinner = function(){
+  console.log(this.totalBank);
+  if(this.totalBank >= 100){
+    console.log("This is working");
+    $('div.win-hide').hide();
+    $('div#winner-output').text("Congratulations! You have won the game! Reload the page to play again")
+  }
 }
+
+// UI Logic
+$(document).ready(function(){
+  let newDice = new Dice();
+  let player1 = new Player('Player 1');
+  let player2 = new Player("Player 2");
+  let total1 = 0;
+  let total2 = 0;
+  $('button#roll-1').on('click', function(){
+    let rollOne = newDice.rollDice();
+    $('div#score-1').text(rollOne);
+    if(rollOne != 1){
+      total1 += rollOne;
+      $('div#total-1').text(total1);
+    } else {
+      alert("You rolled a 1. Lose everything!");
+      $('div#total-1').text(0);
+      $("div#score-1").text('');
+      total1 = 0;
+      $('div#output-1').hide();
+      $('div#output-2').show();
+    }
+  });
+  $('button#roll-2').on('click', function(){
+    let rollTwo = newDice.rollDice();
+    $('div#score-2').text(rollTwo);
+    if(rollTwo != 1){
+      total2 += rollTwo;
+      $('div#total-2').text(total2);
+    } else {
+      alert("You rolled a 1. Lose everything!");
+      $('div#total-2').text(0);
+      $('div#score-2').text('');
+      total2 = 0;
+      $('div#output-2').hide();
+      $('div#output-1').show();
+    }
+  });
+  $('button#hold-1').on('click', function(){
+    player1.totalBank += total1;
+      if(total1 != 0){
+        $('div#output-1').hide();
+        $('div#output-2').show();
+        total1 = 0;
+        $('div#total-1').text(0);
+        $("div#score-1").text('');
+      }
+      player1.checkWinner();
+      console.log(player1)
+  });
+  $('button#hold-2').on('click', function(){
+    player2.totalBank += total2;
+    if(total2 != 0){
+      $('div#output-2').hide();
+      $('div#output-1').show();
+      total2 = 0;
+      $('div#total-2').text(0);
+      $('div#score-2').text('');
+    }
+    player2.checkWinner();
+    console.log(player2)
+  });
+});
+
+
+  
+
+
+
+
 
 // function getRandomInt(min, max) {
 //   min = Math.ceil(min);
 //   max = Math.floor(max);
 //   return Math.floor(Math.random() * (max - min + 1)) + min;
 // }
-
-function Player(name){
-  this.name = name;
-  this.turnScore = 0;
-  this.totalBank = 0;
-
-}
-
-
-
-
-
-
-
-
-
-$(document).ready(function(){
-  let newDice = new Dice();
-  let total1 = 0;
-  let total2 = 0;
-  $('button#roll-1').on('click', function(){
-    let rollOne = newDice.rollDice();
-    $('ul#score-1').append('<li>' + rollOne + '</li>');
-    if(rollOne != 1){
-      total1 += rollOne;
-      $('div#total-1').text(total1);
-    }
-  });
-
-  $('button#roll-2').on('click', function(){
-    let rollTwo = newDice.rollDice();
-    $('ul#score-2').append('<li>' + rollTwo + '</li>');
-    total2 += rollTwo;
-    $('div#total-2').text(total2);
-  });
-});
-
-
-  
